8409e33520b1d6654182098fc33fa922e814f2dd
Joshua Steele <joshukraine@gmail.com>
2017-05-05 22:38:40 +0300

    Require middleman-blog in spec helper.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,10 @@
 require "rspec"
 require "capybara/rspec"
 require "middleman-core"
+require "middleman-blog"
 require "middleman-core/rack"
-# require "middleman-s3_sync"
-# require "middleman-pry"
+require "middleman-s3_sync"
+require "middleman-pry"
 
 middleman_app = ::Middleman::Application.new do
   set :root, File.expand_path(File.join(File.dirname(__FILE__), ".."))
