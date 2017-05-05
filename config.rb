2c29054668bdd75a876b96dbb6257dc9f531fbe8
Joshua Steele <joshukraine@gmail.com>
2017-05-05 23:19:07 +0300

    Set permalink format.

--- config.rb
+++ config.rb
@@ -13,9 +13,9 @@ activate :blog do |blog|
   # This will add a prefix to all links, template references and source paths
   # blog.prefix = "blog"
 
-  # blog.permalink = "{year}/{month}/{day}/{title}.html"
+  blog.permalink = "{year}/{month}/{title}.html"
   # Matcher for blog source files
-  # blog.sources = "{year}-{month}-{day}-{title}.html"
+  blog.sources = "posts/{year}-{month}-{title}.html"
   # blog.taglink = "tags/{tag}.html"
   # blog.layout = "layout"
   # blog.summary_separator = /(READMORE)/
