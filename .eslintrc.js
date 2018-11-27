068b1df498371153eba5b715eca0f9e0cef2ad82
Joshua Steele <joshukraine@gmail.com>
2018-11-27 14:09:31 -0600

    Update eslint config to use Standard.

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,11 @@ module.exports = {
     parser: 'babel-eslint'
   },
   extends: [
-    'plugin:vue/recommended'
+    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+    'plugin:vue/recommended',
+    // https://github.com/standard/standard/blob/master/docs/RULES-en.md
+    'standard'
   ],
   // required to lint *.vue files
   plugins: [
@@ -16,7 +20,16 @@ module.exports = {
   ],
   // add your custom rules here
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    // https://eslint.org/docs/user-guide/configuring#configuring-rules
+    // "off" or 0 - turn the rule off
+    // "warn" or 1 - turn the rule on as a warning (doesn’t affect exit code)
+    // "error" or 2 - turn the rule on as an error (exit code is 1 when triggered)
+    'arrow-parens': 0,
+    'generator-star-spacing': 0,
+    'no-console': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'no-multiple-empty-lines': process.env.NODE_ENV === 'production' ? 2 : 1,
+    'no-trailing-spaces': process.env.NODE_ENV === 'production' ? 2 : 1,
+    'no-unused-vars': process.env.NODE_ENV === 'production' ? 2 : 1
   }
 }
