283366fbbfbc1499cac95e513c4642cb19131c80
Joshua Steele <joshukraine@gmail.com>
2018-11-28 20:58:31 -0600

    First pass at basic navbar layout.

---
+++ components/TheNavbar.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+header.bg-white
+  div.container.py-8.border.border-green.flex.justify-between.items-center
+    nuxt-link.logo(to="/")
+      img(src="~/assets/images/ofr-logo.svg")
+    nav.border.border-blue
+      nuxt-link.ml-6(to="#") Family
+      nuxt-link.ml-6(to="#") Ministry
+      nuxt-link.ml-6(to="#") Archives
+      nuxt-link.ml-6(to="#") Contact
+      nuxt-link.ml-6(to="#") Donate
+</template>
+
+<script>
+export default{
+}
+</script>
+
+<style scoped>
+.logo {
+  display: block;
+  max-width: 300px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,4 @@
 <template lang="pug">
-div
-  h2 This is the home page!
+div.container.border.border-red.text-center
+  h1 This is the home page!
 </template>
