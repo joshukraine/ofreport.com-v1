a055f74c3c0914e94442808d8e710d3e98257f34
Joshua Steele <joshukraine@gmail.com>
2018-11-29 12:51:54 -0600

    Add slide-in animation for navbar on smaller screens.

--- components/TheNavbar.vue
+++ components/TheNavbar.vue
@@ -1,24 +1,32 @@
 <template lang="pug">
-header.bg-white
-  div.container.py-4.flex.justify-between.items-center
+header.nav-header
+  div.container.flex.justify-between.items-center.border.border-green
     nuxt-link.logo.mt-4(to="/")
       img(src="~/assets/images/ofr-logo.svg")
-    nav
-      nuxt-link.nav-link(to="#") Family
-      nuxt-link.nav-link(to="#") Ministry
-      nuxt-link.nav-link(to="#") Archives
-      nuxt-link.nav-link(to="#") Contact
-      nuxt-link.nav-link(to="#") Donate
+    button.nav-button(@click="isActive = !isActive") MENU
+    nav.nav-menu(:class="{ 'reveal-nav': isActive }")
+      div.nav-link-wrapper
+        nuxt-link.nav-link(to="#") Family
+        nuxt-link.nav-link(to="#") Ministry
+        nuxt-link.nav-link(to="#") Archives
+        nuxt-link.nav-link(to="#") Contact
+        nuxt-link.nav-link(to="#") Donate
 </template>
 
 <script>
 export default{
+  data () {
+    return {
+      isActive: false
+    }
+  }
 }
 </script>
 
 <style scoped>
 .logo {
   display: block;
-  max-width: 300px;
+  max-width: 200px;
 }
+
 </style>
