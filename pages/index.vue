30c1e5301263aa23d72786eb0bde723477c02d56
Joshua Steele <joshukraine@gmail.com>
2018-11-30 22:17:27 -0600

    Complete alpha section.

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,26 @@
 <template lang="pug">
-div.container.text-center
-  h1 This is the home page!
-
-  p Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-
-  p Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-
-  p Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-
-  p Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-
-  p Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-
-  p Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-
+div.pt-20(class="lg:pt-24")
+  // Alpha Section
+  section.alpha.bg-cover.bg-top.flex.items-center(class="md:bg-left lg:bg-top")
+    .container.pb-10(class="sm:pb-0 md:py-32")
+      .flex.justify-end.pt-24(class="sm:pt-32 md:pt-0 lg:px-10")
+        .text-center.px-8(class="md:w-2/5 md:px-0")
+          img(class="sm:w-4/5 md:w-full" src="~assets/images/joshua-and-kelsie.svg")
+          p.text-white.font-header.mb-8(class="md:mb-12") Est. 2004
+          p.text-white.font-header.mb-8(class="sm:text-xl md:mb-12") We live in Ukraine where we strive to tell others about God's gift of eternal life through Jesus Christ.
+          button.btn.btn-lg.btn-blue Meet our Family
 </template>
+
+<style>
+.alpha {
+  background-image: url("~assets/images/josh-kels-cover-768w.jpg");
+  min-height: 100vh;
+}
+
+@media (min-width: 768px) {
+  .alpha {
+    background-image: url("~assets/images/josh-kels-cover-2500w.jpg");
+    min-height: 70vh;
+  }
+}
+</style>
