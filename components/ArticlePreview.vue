8879cc237f08d56e288efa3407fb26ca42f5015e
Joshua Steele <joshukraine@gmail.com>
2018-12-09 17:31:37 -0600

    Set up group hover effect for article previews.
    * https://tailwindcss.com/docs/state-variants/#group-hover

--- components/ArticlePreview.vue
+++ components/ArticlePreview.vue
@@ -1,12 +1,11 @@
 <template lang="pug">
 nuxt-link.max-w-md.block.mx-auto(to="#")
-  article.shadow-lg.rounded.overflow-hidden.bg-white.mb-12
-    img(v-if="thumbnail" :src="thumbnail")
-    .px-6.py-4
-      h2 {{ title }}
-      p {{ author }}
-      p {{ pubDate }}
-      p {{ excerpt }}
+  article.group.shadow-lg.rounded.overflow-hidden.bg-white.mb-12
+    img.opacity-85.z-1(class="group-hover:opacity-100" v-if="thumbnail" :src="thumbnail")
+    .preview-body.px-6.pt-2.pb-4
+      h2.text-grey-darkest.mb-0(class="group-hover:text-black") {{ title }}
+      p.text-grey-dark.text-sm {{ author }} &middot; {{ pubDate }}
+      p.text-grey-darkest.mb-0(class="group-hover:text-black") {{ excerpt }}
 </template>
 
 <script>
