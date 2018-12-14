6746db8d426775befbf1ee4ff0057fe0a37e69b1
Joshua Steele <joshukraine@gmail.com>
2018-12-14 08:28:28 +0200

    Correct nested link issue.
    * Realized that when rendering tags, I was nesting anchor tags inside each other.
    * This was causing a mismatch DOM error from Vue. (And it's an HTML no-no.)
    * [Vue warn]: The client-side rendered virtual DOM tree is not matching server-rendered content. This is likely caused by incorrect HTML markup, for example nesting block-level elements inside <p>, or missing <tbody>. Bailing hydration and performing full client-side render.

--- components/ArticlePreview.vue
+++ components/ArticlePreview.vue
@@ -1,15 +1,16 @@
 <template lang="pug">
-nuxt-link.max-w-md.block.mx-auto(to="#")
-  article.group.shadow-md.rounded.overflow-hidden.bg-white.mb-12
+article.max-w-md.mx-auto.group.shadow-md.rounded.overflow-hidden.bg-white.mb-12
+  nuxt-link(to="#")
     img.opacity-90.z-1(class="group-hover:opacity-100" v-if="thumbnail" :src="thumbnail")
-    .px-6.pt-2.pb-4
+  .px-6.pt-2.pb-4
+    nuxt-link(to="#")
       h2.text-black.mb-0(class="hover:text-ofr-blue") {{ title }}
-      p.text-grey-dark.text-sm {{ author }} &middot; {{ pubDate }}
-      p.text-grey-darkest.mb-0 {{ excerpt }}
-    div.px-6.py-4(v-if="tags.length > 0")
-      span.inline-block.bg-grey-lighter.rounded-full.px-3.py-1.text-sm.font-semibold.text-grey-darker.mr-2(
-        v-for="(tag, index) in tags" :key="index")
-        nuxt-link(to="#") &#35;{{ tag }}
+    p.text-grey-dark.text-sm {{ author }} &middot; {{ pubDate }}
+    p.text-grey-darkest.mb-0 {{ excerpt }}
+  div.px-6.py-4(v-if="tags.length > 0")
+    span.inline-block.bg-grey-lighter.rounded-full.px-3.py-1.text-sm.font-semibold.text-grey-darker.mr-2(
+      v-for="(tag, index) in tags" :key="index")
+      nuxt-link(to="#") &#35;{{ tag }}
 </template>
 
 <script>
