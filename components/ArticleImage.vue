c7eee2324260f477556c62d1b6853d85f8203245
Joshua Steele <joshukraine@gmail.com>
2018-12-25 19:16:27 +0200

    Add custom classes, other enhancements.

--- components/ArticleImage.vue
+++ components/ArticleImage.vue
@@ -1,8 +1,16 @@
 <template lang="pug">
-div.text-center.my-12(v-editable="segment")
-  a(:href="segment.large")
-    img.rounded(:src="segment.medium" :alt="segment.caption")
-  .max-w-sm.mx-auto
+.article-image.text-center.my-12(v-editable="segment")
+  a(v-if="segment.link" :href="segment.link")
+    img.rounded(
+      :src="segment.src"
+      :class="segment.classes"
+      :alt="segment.caption")
+  img.rounded(
+    v-else
+    :src="segment.src"
+    :class="segment.classes"
+    :alt="segment.caption")
+  .max-w-sm.mx-auto(v-if="segment.caption")
     p.font-bold {{ segment.caption }}
 </template>
 
