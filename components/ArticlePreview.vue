0dce89e6f1c6d8a3a30c495b1d5d8432e2f6866e
Joshua Steele <joshukraine@gmail.com>
2018-12-25 20:19:25 +0200

    Improve tag spacing.

--- components/ArticlePreview.vue
+++ components/ArticlePreview.vue
@@ -8,8 +8,10 @@ article.max-w-md.mx-auto.group.shadow-md.rounded.overflow-hidden.bg-white.mb-12
     p.text-grey-dark.text-sm {{ author }} &middot; {{ pubDate }}
     p.text-grey-darkest.mb-0 {{ excerpt }}
   div.px-6.py-4(v-if="tags.length > 0")
-    span.inline-block.bg-grey-lighter.rounded-full.px-3.py-1.text-sm.font-semibold.text-grey-darker.mr-2(
-      v-for="(tag, index) in tags" :key="index")
+    span.inline-block.bg-grey-lighter.rounded-full.px-3.py-1.text-xs.font-semibold.text-grey-darker.mr-2.mb-2(
+      v-for="(tag, index) in tags"
+      :key="index"
+      class="sm:text-sm md:mb-0")
       nuxt-link(to="#") &#35;{{ tag }}
 </template>
 
