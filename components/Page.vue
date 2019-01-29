74b866d1f04b445b5528d9e7558d5817bd3116bb
Joshua Steele <joshukraine@gmail.com>
2019-01-29 14:09:45 +0200

    Rename body property to segments.

--- components/Page.vue
+++ components/Page.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
 div(v-editable="blok" class="page")
   component(
-    v-for="blok in blok.body"
+    v-for="blok in blok.segments"
     :key="blok._uid"
     :blok="blok"
     :is="blok.component")
