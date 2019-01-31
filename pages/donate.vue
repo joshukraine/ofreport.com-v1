3ad9741bf12d0c8fdf2f558c39aadada2ea23a38
Joshua Steele <joshukraine@gmail.com>
2019-01-31 08:47:57 +0200

    Add remaining page stubs.

---
+++ pages/donate.vue
@@ -0,0 +1,42 @@
+<template lang="pug">
+.container(v-editable="story.content")
+  h1.text-center {{ story.content.title }}
+
+  component(
+    v-if="story.content.component"
+    :key="story.content._uid"
+    :blok="story.content"
+    :is="story.content.component")
+</template>
+
+<script>
+import storyblokLivePreview from '@/mixins/storyblokLivePreview'
+
+export default {
+  mixins: [
+    storyblokLivePreview
+  ],
+  data () {
+    return {
+      story: {
+        content: {
+          title: ''
+        }
+      }
+    }
+  },
+  asyncData (context) {
+    let version = context.query._storyblok || context.isDev ? 'draft' : 'published'
+    let endpoint = 'cdn/stories/donate'
+
+    return context.app.$storyapi.get(endpoint, {
+      version: version
+    }).then((res) => {
+      console.log(res.data)
+      return res.data
+    }).catch((res) => {
+      context.error({ statusCode: res.response.status, message: res.response.data })
+    })
+  }
+}
+</script>
